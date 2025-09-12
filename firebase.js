9061fbe521ef181070e1ad9875cf8e953ca083e3
Harshil8136 <harshil.8136@gmail.com>
2025-09-12 00:32:52 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyDnCDVc-",
+    apiKey: "TEST_KEY_ID_BLANK",
     authDomain: "biller-hub-analytics.firebaseapp.com",
     projectId: "biller-hub-analytics",
     storageBucket: "biller-hub-analytics.firebasestorage.app",
     messagingSenderId: "529544782831",
-    appId: "1:529544782831:web:7176aa745ba56b6dd80c1a",
+    appId: "AppID_BLANK",
     measurementId: "G-440WP7X6PF"
   };
 
@@ -22,3 +22,4 @@
   const analytics = getAnalytics(app);
 
 </script>
+
