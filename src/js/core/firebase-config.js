8b503aea362378d08149428987ea4acb38677ef1
Harshil8136 <harshil.8136@gmail.com>
2025-09-12 00:31:36 -0400

    Update firebase-config.js

--- src/js/core/firebase-config.js
+++ src/js/core/firebase-config.js
@@ -7,11 +7,12 @@
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnCDVc-PUK7s72jyXunt50kKZf2eIBfy8",
+  apiKey: "AIzaSyDnCDVc-",
   authDomain: "biller-hub-analytics.firebaseapp.com",
   projectId: "biller-hub-analytics",
   storageBucket: "biller-hub-analytics.firebasestorage.app",
   messagingSenderId: "529544782831",
   appId: "1:529544782831:web:7176aa745ba56b6dd80c1a",
   measurementId: "G-440WP7X6PF"
+
 };
