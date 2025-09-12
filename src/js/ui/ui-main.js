bf0ae4a0e180ae1f0eafc9196c3629e21e655181
Harshil8136 <harshil.8136@gmail.com>
2025-09-12 00:37:47 -0400

    Delete src directory

--- src/js/ui/ui-main.js
+++
@@ -1,9 +0,0 @@
-/**
- * UI MAIN
- * ==================
- * This file creates the global UI object, which serves as a namespace for all
- * other UI modules to attach their components. It must be the first UI
- * script loaded.
- */
-
-const UI = {};
